13c8a07f860d34b6579142601ec8561cd43ddbcc
hungrykirby <katsuki8hanawa6@gmail.com>
2023-04-08 23:11:06 +0900

    add text of image index below image

--- twitter_pictures_gui/lib/main.dart
+++ twitter_pictures_gui/lib/main.dart
@@ -118,26 +118,42 @@ class _MyHomePageState extends State<MyHomePage> {
       ),
       body: Container(
         height: 500.0,
-        width: 1000.0,
+        width: 800.0,
         padding: const EdgeInsets.all(5.0),
         decoration: BoxDecoration(
           color: Color.fromARGB(255, 94, 94, 94),
           border: Border.all(color: Color.fromARGB(255, 40, 179, 102), width: 3),
         ),
-        child: CarouselSlider(
-          items: _images,
-          key: UniqueKey(),
-          options: CarouselOptions(
-            autoPlay: false,
-            initialPage: _imageIndex,
-            enableInfiniteScroll: true,
-            onPageChanged: (index, reason) {
-              setState(() {
-                _imageIndex = index;
-              });
-            },
-          ),
-        ),
+        child: Column(
+          children: [
+            CarouselSlider(
+              items: _images,
+              key: UniqueKey(),
+              options: CarouselOptions(
+                autoPlay: false,
+                initialPage: _imageIndex,
+                enableInfiniteScroll: true,
+                onPageChanged: (index, reason) {
+                  setState(() {
+                    _imageIndex = index;
+                  });
+                },
+              ),
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Text(
+                  '$_imageIndex',
+                  style: TextStyle(
+                    fontSize: 20,
+                    color: Colors.white
+                  ),
+                )
+              ],
+            )
+          ],
+        ) 
       ),
     );
   }

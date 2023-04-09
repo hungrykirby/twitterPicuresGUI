2332a64f72d2eb3a7d85827ae23701653160a7ee
hungrykirby <hungrykirby@users.noreply.github.com>
2023-04-09 20:08:03 +0900

    enable arrow input (#4)

--- twitter_pictures_gui/lib/main.dart
+++ twitter_pictures_gui/lib/main.dart
@@ -9,6 +9,10 @@ import 'dart:io';
 // 環境変数
 import 'package:flutter_dotenv/flutter_dotenv.dart';
 
+// import 'package:flutter/foundation.dart';
+// enable keyboard input
+import 'package:flutter/services.dart';
+
 Future<void> main() async {
   await dotenv.load();
   
@@ -117,6 +121,10 @@ class _MyHomePageState extends State<MyHomePage> {
   final imageFrameBackgroundColor = const Color.fromARGB(255, 94, 94, 94);
   final imageBorderColor = const Color.fromARGB(255, 40, 179, 102);
   final imageIndexStyle = const TextStyle(fontSize: 20, color: Colors.white);
+  final imageFilePathStyle = const TextStyle(fontSize: 12, color: Colors.white);
+
+  DateTime _lastEventTime = DateTime(0); // 最後に処理されたイベントの時刻 キーボード入力が重複するため回避策
+  final keyDuration = const Duration(milliseconds: 200);
 
   @override
   Widget build(BuildContext context) {
@@ -124,41 +132,77 @@ class _MyHomePageState extends State<MyHomePage> {
       appBar: AppBar(
         title: Text(widget.title),
       ),
-      body: Container(
-        height: 500.0,
-        width: 800.0,
-        padding: const EdgeInsets.all(5.0),
-        decoration: BoxDecoration(
-          color: imageFrameBackgroundColor,
-          border: Border.all(color: imageBorderColor, width: 3),
-        ),
-        child: Column(
-          children: [
-            CarouselSlider(
-              items: _images,
-              key: UniqueKey(),
-              options: CarouselOptions(
-                autoPlay: false,
-                initialPage: _imageIndex,
-                enableInfiniteScroll: true,
-                onPageChanged: (index, reason) {
-                  setState(() {
-                    _imageIndex = index;
-                  });
-                },
+      body: RawKeyboardListener(
+        focusNode: FocusNode(),
+        onKey: (RawKeyEvent event) {
+           final currentTime = DateTime.now();
+            final deltaTime = currentTime.difference(_lastEventTime);
+            if (deltaTime > keyDuration) {
+              if (event.logicalKey == LogicalKeyboardKey.arrowLeft) {
+                setState(() {
+                  _imageIndex = (_imageIndex - 1) % _images.length;
+                });
+              } else if (event.logicalKey == LogicalKeyboardKey.arrowRight) {
+                setState(() {
+                  _imageIndex = (_imageIndex + 1) % _images.length;
+                });
+              } else if (event.logicalKey == LogicalKeyboardKey.arrowUp) {
+                setState(() {
+                  _imageIndex = 0;
+                });
+              } else if (event.logicalKey == LogicalKeyboardKey.arrowDown) {
+                setState(() {
+                  _imageIndex = _images.length - 1;
+                });
+              }
+              _lastEventTime = currentTime;
+            }
+        },
+        child: Container(
+          height: 500.0,
+          width: 800.0,
+          padding: const EdgeInsets.all(5.0),
+          decoration: BoxDecoration(
+            color: imageFrameBackgroundColor,
+            border: Border.all(color: imageBorderColor, width: 3),
+          ),
+          child: Column(
+            children: [
+              CarouselSlider(
+                items: _images,
+                key: UniqueKey(),
+                options: CarouselOptions(
+                  autoPlay: false,
+                  initialPage: _imageIndex,
+                  enableInfiniteScroll: true,
+                  onPageChanged: (index, reason) {
+                    setState(() {
+                      _imageIndex = index;
+                    });
+                  },
+                ),
               ),
-            ),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                Text(
-                  '$_imageIndex',
-                  style: imageIndexStyle,
-                )
-              ],
-            )
-          ],
-        ) 
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Text(
+                    '$_imageIndex',
+                    style: imageIndexStyle,
+                  )
+                ],
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Text(
+                    _tweet[_imageIndex][5],
+                    style: imageFilePathStyle,
+                  )
+                ],
+              )
+            ],
+          )
+        )
       ),
     );
   }
